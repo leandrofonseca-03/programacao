0de5c1fd1ec81d7de066e3b919ec687b90bf099b
leandrofonseca-03 <54272514+leandrofonseca-03@users.noreply.github.com>
2019-09-17 16:35:21 -0300

    Exercício

---
+++ src/com/Unama/letraH.java
@@ -0,0 +1,19 @@
+package com.Unama;
+
+import java.util.Scanner;
+
+public class letraH {
+    public static void main(String[] args){
+        Scanner leia = new Scanner(System.in);
+            double salarioAtual, reajuste, salarioReajus;
+            System.out.print("Cálculo do reajuste salarial do servidor.");
+            System.out.println(" ");
+            System.out.println(" ");
+            System.out.print("Insira seu salário atual: ");
+            salarioAtual = leia.nextDouble();
+            System.out.print("Insira o valor do reajuste(ex.: para 10% digite 0.1: ");
+            reajuste = leia.nextDouble();
+
+
+    }
+}
