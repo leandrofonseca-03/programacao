0a32cb25ffd50a3d5ca6f5bfb55bb40406ca04b7
leandrofonseca-03 <54272514+leandrofonseca-03@users.noreply.github.com>
2019-09-25 16:32:39 -0300

    Lista 2

---
+++ src/com/Unama/Ex2letraJ.java
@@ -0,0 +1,10 @@
+package com.Unama;
+
+import java.util.Scanner;
+
+public class Ex2letraJ {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+    }
+}
