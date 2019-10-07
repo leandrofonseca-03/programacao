09ff1bc9bbd73a256bdca60bf4bbbaed229ec5ff
leandrofonseca-03 <54272514+leandrofonseca-03@users.noreply.github.com>
2019-10-07 10:11:58 -0300

    Lista 2

--- src/com/Unama/Ex2letraJ.java
+++ src/com/Unama/Ex2letraJ.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class Ex2letraJ {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        double n1,n2,soma,raiz;
+        int escolha;
+        soma = 1;
+        raiz = 2;
+        System.out.println("Escolha qual operação quer realizar: ");
+        System.out.println("1 - Para soma;");
+        System.out.println("2 - Para raiz quadrada.");
+        escolha = leia.nextInt();
+        if(escolha==1){
+            System.out.print("Insira o primeiro número: ");
+            n1 = leia.nextDouble();
+            System.out.print("Insira o segundo número: ");
+            n2 = leia.nextDouble();
+            soma = n1+n2;
+            System.out.print("A soma dos dois números é "+soma);
+        }else if (escolha==2){
+            System.out.print("Insira o número: ");
+            n1 = leia.nextDouble();
+            System.out.println("A raiz quadrada é "+Math.sqrt(n1));
+
+        }
+
 
     }
 }
